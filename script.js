7f6c16a71ec8b25df3686d5c8fc6511442f9637d
KenningSay <142360863+KenningSay@users.noreply.github.com>
2026-02-17 14:28:56 +0900

    ыв

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("Скрипт подключен, ебана народ погнали нахуй!!!");
+
+let knopka = document.querySelector('.js_button');
+let frazy = ["АХАХА!", "ЕЩЁ!", "ХВАТИТ УЖЕ!", "ТЫ МОНСТР!"];
+let emoji = ["💥", "🔥", "⚡", "🍆", "😈", "🤘", "💀"];
+
+
+
+console.log(knopka);
+
+// Событие на клик
+knopka.onclick = function(){
+    let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.background = color;
+
+    let nomer = Math.floor(Math.random() * frazy.length);
+    
+    console.log(frazy[nomer]);
+    console.log("КНОПКА НАЖАТА, СУЧАРА!");
+
+    knopka.textContent = frazy[nomer];
+    for (let i = 0; i < 50; i++){
+    
+    let newDiv = document.createElement('div');
+    let ranX = Math.floor(Math.random() * window.innerWidth);
+    let num = Math.floor(Math.random() * emoji.length);
+    let ranY = Math.floor(Math.random() * window.innerHeight);
+    newDiv.textContent = emoji[num];
+    newDiv.style.position = 'fixed';
+    newDiv.style.left = ranX + 'px';
+    newDiv.style.top = ranY + 'px';
+    newDiv.style.fontSize = '40px';
+    document.body.appendChild(newDiv);
+    }
+    
+}
+
+
